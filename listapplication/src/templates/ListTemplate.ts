abdadb6e2c4978e49420498301a827e5e43a3b40
njiutheguru <stephennjiu@gmail.com>
2025-04-06 01:45:12 +0300

    list-application

---
+++ listapplication/src/templates/ListTemplate.ts
@@ -0,0 +1,62 @@
+import FullList from "../model/FullList";
+
+interface DOMList {
+    ul: HTMLUListElement,
+    clear(): void,
+    render(FullList:FullList): void,
+    
+}
+
+export default class ListTemplate implements DOMList {
+    ul: HTMLUListElement
+    text: HTMLInputElement
+
+    static instance: ListTemplate = new ListTemplate()
+
+    private constructor() {
+        this.ul = document.getElementById("listItems") as HTMLUListElement
+        this.text = document.getElementById("newItem") as HTMLInputElement
+    }
+    clear(): void {
+        this.ul.innerHTML = ''
+        this.text.innerHTML = ''
+    }
+
+    render(fullList: FullList): void {
+        this.clear()
+        fullList.list.forEach(item => {
+            const li = document.createElement("li") as HTMLLIElement
+            li.className = 'item'
+
+            const check = document.createElement("input") as HTMLInputElement
+            check.type = "checkbox"
+            check.id = item.id
+            check.tabIndex = 0 
+            check.checked = item.checked 
+            li.append(check)
+
+            check.addEventListener('change', () => {
+                item.checked = !item.checked
+                fullList.save()
+            })
+            
+            const label = document.createElement("labael") as HTMLLabelElement
+            label.htmlFor = item.id
+            label.textContent = item.item 
+            li.append(label)
+
+
+            const button = document.createElement("button") as HTMLButtonElement 
+            button.className = 'button'
+            button.textContent = 'X'
+            li.append(button)
+
+            button.addEventListener("click", () =>{
+                fullList.removeItem(item.id)
+                this.render(fullList)
+            })
+
+            this.ul.append(li)
+        })
+    }
+}
